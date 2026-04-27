4f7f0e828337d2fb0ac2dd3bd772d28c1b2cdc1a
Paing Thet Ko <paingmi004@gmail.com>
2026-04-27 13:43:10 +0630

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationService } from './core/services/notification.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('angular-firebase');
+  protected readonly title = signal('Secondtime-Angular-Firebase');
+
+  constructor(private notification: NotificationService) {}
+
+  ngOnInit(): void {
+    this.notification.requestPermission();
+
+    this.notification.listen();
+  }
 }

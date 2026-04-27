3eea85fdd69d20d27ba242f426be051139d68860
Paing Thet Ko <paingmi004@gmail.com>
2026-04-27 13:43:04 +0630

    Create firebase.config.ts

---
+++ src/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDPRhtA8dcjMdT6yFZx0i62d5PGDONZBRY',
+  authDomain: 'angular-firebase-93df0.firebaseapp.com',
+  projectId: 'angular-firebase-93df0',
+  storageBucket: 'angular-firebase-93df0.firebasestorage.app',
+  messagingSenderId: '29552555542',
+  appId: '1:29552555542:web:afed88f23b367fdd816737',
+  measurementId: 'G-G149DH9NTL',
+};
